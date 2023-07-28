86900e3ac493d0087bb78a2dde956ca0a8bd88ee
Beluuuuuuga <koki.inoue@elith.co.jp>
2023-07-28 08:53:00 +0000

    add version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='langdechat',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/yourusername/langdechat',
     author='beluuuuuuga',
     author_email='koki.inoue@elith.co.jp',
